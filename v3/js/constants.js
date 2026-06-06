4bb62d70f5efb9a89a9406b0946e7d05b1809870
mantanak-jp <mantanak@gmail.com>
2026-06-07 08:27:14 +0900

    Export V3 constants module

--- v3/js/constants.js
+++ v3/js/constants.js
@@ -1,46 +1,42 @@
-(function() {
-  "use strict";
+export const USER_OPTIONS = {
+  ryoma: { label: "りょうま" },
+  sara: { label: "さら" },
+  guest: { label: "ゲスト" }
+};
 
-  const USER_OPTIONS = {
-    ryoma: { label: "りょうま" },
-    sara: { label: "さら" },
-    guest: { label: "ゲスト" }
-  };
+export const GENERATION_NAMES = [
+  "",
+  "第1世代（カントー）",
+  "第2世代（ジョウト）",
+  "第3世代（ホウエン）",
+  "第4世代（シンオウ）",
+  "第5世代（イッシュ）",
+  "第6世代（カロス）",
+  "第7世代（アローラ）",
+  "第8世代（ガラル）",
+  "第9世代（パルデア）"
+];
 
-  const GENERATION_NAMES = [
-    "",
-    "第1世代（カントー）",
-    "第2世代（ジョウト）",
-    "第3世代（ホウエン）",
-    "第4世代（シンオウ）",
-    "第5世代（イッシュ）",
-    "第6世代（カロス）",
-    "第7世代（アローラ）",
-    "第8世代（ガラル）",
-    "第9世代（パルデア）"
-  ];
+export const GENERATION_REGION_NAMES = [
+  "",
+  "カントー",
+  "ジョウト",
+  "ホウエン",
+  "シンオウ",
+  "イッシュ",
+  "カロス",
+  "アローラ",
+  "ガラル",
+  "パルデア"
+];
 
-  const GENERATION_REGION_NAMES = [
-    "",
-    "カントー",
-    "ジョウト",
-    "ホウエン",
-    "シンオウ",
-    "イッシュ",
-    "カロス",
-    "アローラ",
-    "ガラル",
-    "パルデア"
-  ];
+export const QUIZ_QUESTION_COUNT = 5;
+export const QUIZ_NEXT_DELAY_MS = 1200;
 
-  const QUIZ_QUESTION_COUNT = 5;
-  const QUIZ_NEXT_DELAY_MS = 1200;
-
-  window.AppConstants = {
-    USER_OPTIONS,
-    GENERATION_NAMES,
-    GENERATION_REGION_NAMES,
-    QUIZ_QUESTION_COUNT,
-    QUIZ_NEXT_DELAY_MS
-  };
-})();
+window.AppConstants = {
+  USER_OPTIONS,
+  GENERATION_NAMES,
+  GENERATION_REGION_NAMES,
+  QUIZ_QUESTION_COUNT,
+  QUIZ_NEXT_DELAY_MS
+};
