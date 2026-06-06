fd3043e7a5beeee4019e52824ee513be32ad9174
mantanak-jp <mantanak@gmail.com>
2026-06-07 08:32:59 +0900

    Export V3 state module

--- v3/js/state.js
+++ v3/js/state.js
@@ -1,31 +1,29 @@
-(function() {
-  "use strict";
+export const state = {
+  db: null,
+  firebaseReady: false,
+  selectedUserId: null,
+  selectedUserLabel: "",
+  currentUserData: null,
+  currentGeneration: null,
+  currentGenerationPokemonList: null,
+  currentGenerationMasterPromise: null,
+  currentGenerationMasterError: null,
+  isLoadingUser: false,
+  selectedLevel: 1,
+  currentQuestionIndex: 0,
+  correctCount: 0,
+  currentCorrectAnswer: 0,
+  isAnswered: false,
+  quizTimerId: null,
+  pendingRewardPokemon: [],
+  pendingRewardPokemonList: [],
+  isSavingReward: false,
+  lastProgressResult: null,
+  generationStartLevel: 1,
+  generationStartTarget: null,
+  selectedZukanGeneration: null,
+  generationMasterCache: {},
+  generationMasterPromises: {}
+};
 
-  window.AppState = {
-    db: null,
-    firebaseReady: false,
-    selectedUserId: null,
-    selectedUserLabel: "",
-    currentUserData: null,
-    currentGeneration: null,
-    currentGenerationPokemonList: null,
-    currentGenerationMasterPromise: null,
-    currentGenerationMasterError: null,
-    isLoadingUser: false,
-    selectedLevel: 1,
-    currentQuestionIndex: 0,
-    correctCount: 0,
-    currentCorrectAnswer: 0,
-    isAnswered: false,
-    quizTimerId: null,
-    pendingRewardPokemon: [],
-    pendingRewardPokemonList: [],
-    isSavingReward: false,
-    lastProgressResult: null,
-    generationStartLevel: 1,
-    generationStartTarget: null,
-    selectedZukanGeneration: null,
-    generationMasterCache: {},
-    generationMasterPromises: {}
-  };
-})();
+window.AppState = state;
